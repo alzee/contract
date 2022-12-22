84466f50dbd6783441dcee09b91c7537a4220e1c
Al Zee <z@alz.ee>
2022-12-22 16:47:56 +0800

    nth

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +14,21 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield
+            TextField::new('username')->OnlyWhenUpdating()->setFormTypeOptions(['disabled' => 'disabled']);
+        yield TextField::new('username')->HideWhenUpdating();
+        yield ChoiceField::new('roles')
+            ->setChoices([
+                'Marketing' => 'ROLE_MARKEING',
+                'Finance' => 'ROLE_FINANCE',
+                'General' => 'ROLE_GENERAL',
+                'Business' => 'ROLE_BUSINESS',
+                'Admin' => 'ROLE_ADMIN',
+            ])
+            ->allowMultipleChoices()
+            // ->onlyOnIndex()
+        ;
     }
-    */
 }
