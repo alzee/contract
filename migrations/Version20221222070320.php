182de5cf294a2ccf5b4556c366f311cedf4b0ba7
Al Zee <z@alz.ee>
2022-12-22 15:06:01 +0800

    entity feedback

---
+++ migrations/Version20221222070320.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221222070320 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE feedback (id INT AUTO_INCREMENT NOT NULL, contract_id INT NOT NULL, problem VARCHAR(255) NOT NULL, date DATE NOT NULL, score INT NOT NULL, INDEX IDX_D22944582576E0FD (contract_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE feedback ADD CONSTRAINT FK_D22944582576E0FD FOREIGN KEY (contract_id) REFERENCES contract (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE feedback DROP FOREIGN KEY FK_D22944582576E0FD');
+        $this->addSql('DROP TABLE feedback');
+    }
+}
---
+++ src/Entity/Feedback.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FeedbackRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FeedbackRepository::class)]
+class Feedback
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Contract $contract = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $problem = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?int $score = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContract(): ?Contract
+    {
+        return $this->contract;
+    }
+
+    public function setContract(?Contract $contract): self
+    {
+        $this->contract = $contract;
+
+        return $this;
+    }
+
+    public function getProblem(): ?string
+    {
+        return $this->problem;
+    }
+
+    public function setProblem(string $problem): self
+    {
+        $this->problem = $problem;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+}
